42b2bbafe35364b846d3449870cca53acf8dc9b8
Dhanishsekar <103979971+Dhanishsekar@users.noreply.github.com>
2026-03-28 23:52:13 +0530

    Update images.js

--- images.js
+++ images.js
@@ -8,13 +8,28 @@ const VEHICLE_IMAGES = {
   INNOVA: "img/INNOVA.webp",
 };
 
+const ROUTE_IMAGES = {
+  kodairoad:   "img/kodairoad.jpg",
+  dindigul:    "img/Dindigul.jpg",
+  madurai:     "img/madurai.jpg",
+  coimbatore:  "img/coimbatore.jpg",
+  trichy:      "img/trichy.jpg",
+  cochin:      "img/cochin.jpg",
+  munnar:      "img/munnar.jpg",
+  chennai:     "img/chennai.jpg",
+  kanyakumari: "img/kanyakumari.jpg",
+};
+
 document.addEventListener("DOMContentLoaded", function () {
+  // Hero slides
   const slides = document.querySelectorAll(".cslide[data-slide]");
   slides.forEach(function (slide, index) {
     if (HERO_SLIDES[index]) {
       slide.style.background = "url('" + HERO_SLIDES[index] + "') center/cover no-repeat";
     }
   });
+
+  // Vehicle images
   Object.entries(VEHICLE_IMAGES).forEach(function ([key, src]) {
     const el = document.querySelector('[data-vehicle="' + key + '"]');
     if (el) { el.src = src; }
